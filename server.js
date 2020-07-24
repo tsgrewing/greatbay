4ee9c73c5cc1f731e2d9ef6160069510f1a0a295
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-07-23 21:07:37 -0500

    adding inquirer

--- server.js
+++ server.js
@@ -25,16 +25,39 @@ connection.connect(function(err) {
 ​
 // function which prompts the user for what action they should take
 function start() {
-​
+    // ​   inquirer questions for bid or post
+    return inquirer.prompt({
+        type: "checkbox",
+        name: "option",
+        message: "Would you like to post an item or bid on an item?",
+        choices: ["post", "bid"]
+    }).then(answers => {
+        if (answers.option === "post") {
+            postAuction();
+        }
+        else {
+            bidAuction();
+        }
+    })
+
 }
 ​
 // function to handle posting new items up for auction
 function postAuction() {
   // prompt for info about the item being put up for auction
-​
+​   return inquirer.prompt([{
+    type: "input",
+    name: "name",
+    message: "What would you like to post?"
+}, 
+]).then(answers => {
+    
 }
+
 ​
 function bidAuction() {
   // query the database for all items being auctioned
-​
+​    
+
+
 }
